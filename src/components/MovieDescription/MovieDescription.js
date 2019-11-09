b93ff086fdcdbb89ddf6ca63babf9d0986a2d614
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-11-09 16:09:05 -0600

    added button to route back to list from details page

--- src/components/MovieDescription/MovieDescription.js
+++ src/components/MovieDescription/MovieDescription.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
 const mapReduxStateToProps = reduxState => ({
     reduxState
@@ -13,15 +15,20 @@ class MovieDescription extends Component {
       this.props.history.push('/edit')
   }
 
+  backToList = () => {
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <div class ="MovieDescription">
+          <Button onClick={this.backToList}><ArrowBackRoundedIcon fontSize="large" /></Button>
           <h2>{this.props.reduxState.movieInfo.title}</h2>
           <img src={this.props.reduxState.movieInfo.poster} alt={this.props.reduxState.movieInfo.title}/> < br/>
           {this.props.reduxState.movieGenres.map((genre) => 
             <span class="genreSpan" key={genre.name}>{genre.name}</span>)}
           <p>{this.props.reduxState.movieInfo.description}</p>
-          <button onClick={() => this.editInfoPage(this.props.reduxState.movieInfo)}>Edit Info</button>
+          <Button onClick={() => this.editInfoPage(this.props.reduxState.movieInfo)}>Edit Info</Button>
           <pre>{JSON.stringify(this.props.reduxState.movieGenres)}</pre>
       </div>
     );
